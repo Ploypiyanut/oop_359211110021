47003a34906813aa71da9ebcb9cdd2fe1f39b4cd
PloyPiyanut <inukson2538@gmail.com>
2017-11-27 15:40:13 +0700

    Test Calgrede

--- src/ooplab3/CalGrade.java
+++ src/ooplab3/CalGrade.java
@@ -1,9 +1,34 @@
 package ooplab3;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CalGrade {
-}
- //            score 0-49 grade F
- //            score 50-59 grade D
- //            score 60-69 grade c
- //            score 70-79 grade B
- //            score 80-100 grade A
+    //            score 0-49 grade F
+    //            score 50-59 grade D
+    //            score 60-69 grade c
+    //            score 70-79 grade B
+    //            score 80-100 grade A
+
+    public static void main(String[] args) throws IOException {
+        // BufferedReader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter your score:");
+        int score = Integer.parseInt(reader.readLine());
+
+        if (score<50) System.out.println("you grade: F");
+        else if (score<=59) System.out.println("you grade: D");
+        else if (score<=69) System.out.println("you grade: C");
+        else if (score<=79) System.out.println("you grade: B");
+        else System.out.println("you grade: A");
+
+    }//main
+
+} //class
+
+
+
+
+
---
+++ src/ooplab3/TeatSwitch.java
@@ -0,0 +1,30 @@
+package ooplab3;
+
+import java.util.Scanner;
+
+public class TeatSwitch {
+    public static void main(String[] args) {
+        int select;
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter number between 1-3");
+//        select = scanner.nextInt();
+//        // switch-case
+//        switch (select) {
+//            case 1: System.out.println("your enter 1"); break;
+//            case 2: System.out.println("your enter 2"); break;
+//            case 3: System.out.println("your enter 3"); break;
+//            default:System.out.println("please enter number between 1-3,Thank you1-3");
+//        }//switch
+        char c = 'A';
+        switch (c) {
+            case 'A': System.out.println("A ant");
+            case 'B': System.out.println("A ant");
+            case 'C': System.out.println("A ant");
+        }//switch
+
+
+    }//main
+
+
+
+} //main
