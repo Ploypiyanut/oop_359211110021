5203e2b54adc35d0e815c8b17627d905e23501f9
PloyPiyanut <inukson2538@gmail.com>
2017-12-04 15:46:54 +0700

    local variable

---
+++ src/ooplap4/OverloadMethod.java
@@ -0,0 +1,29 @@
+package ooplap4;
+
+public class OverloadMethod {
+    private static int value = 100;
+
+    public static void A(){
+        System.out.println("Hello");
+        int number = 20;
+        System.out.println(number);
+        System.out.println(value);
+    }
+    public static void A(int x) {
+        System.out.println("Hello"+x);
+    }
+    public static void A(int x,int y) {
+        System.out.println("Hello"+(x+y));
+    }
+
+
+    public static void main(String[] args) {
+//        OverloadMethod
+        A(5,10);
+
+//        local variable
+        int number = 10;
+
+
+    }//main
+}//class
