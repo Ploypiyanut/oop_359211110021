b64702cae97cb6cd3043e42f4f68af842aef8a4c
PloyPiyanut <inukson2538@gmail.com>
2017-12-04 14:56:37 +0700

    Netst Loop

---
+++ src/ooplab3/Testwhileloop.java
@@ -0,0 +1,22 @@
+package ooplab3;
+
+public class Testwhileloop {
+    public static void main(String[] args) {
+        // main
+               int n = 1;
+        while (n <= 10) {
+            System.out.println("Hello");
+            n++;
+
+        } //while
+
+        // do--while
+        do {
+            System.out.println("Hello");
+            n++;
+        } while (n<=10);
+
+
+    }  // main
+
+}//class
