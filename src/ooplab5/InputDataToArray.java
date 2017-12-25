6f52bfffa570ab7a942f6564a9c6dbed1e146be0
PloyPiyanut <inukson2538@gmail.com>
2017-12-25 14:47:05 +0700

    Array2D

--- src/ooplab5/InputDataToArray.java
+++ src/ooplab5/InputDataToArray.java
@@ -11,8 +11,19 @@ public class InputDataToArray {
         Integer[] num = new Integer[MAX];
         // int [] num2 =new int [MAX];
         num = inputData(num);
-
+        showData(num);
     } // main
+    private static void ShowData(Integer[] num) {
+        System.out.println("Data in array:");
+        for (int i=0; i<num.length;i++) {
+            System.out.print(num[i]+"");
+        }
+// enhance loop
+        for (int i : num) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
 
     private static Integer[] inputData(Integer[] num) {
         Scanner scanner = new Scanner(System.in);
@@ -26,12 +37,10 @@ public class InputDataToArray {
         return num;
     } // inputData
 
-    private static void ShowData(Integer[] num) {
+     enhance loprivate static void ShowData(Integer[] num) {
         System.out.println("Data in array:");
-//        for (int i=0; i<num.length;i++) {
-//            System.out.print(num[i]+"");
-//        }
-       // enhance loop
+        for (int i=0; i<num.length;i++)
+        //op
         for (int i : num) {
             System.out.print(i+" ");
         }
