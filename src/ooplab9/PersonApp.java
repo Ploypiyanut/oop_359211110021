276760f3035f1029ebfaea4263adfb0783ddde89
Ploypiyanut <Inukson2538@gmail.com>
2018-02-05 15:13:13 +0700

    Address

---
+++ src/ooplab9/PersonApp.java
@@ -0,0 +1,14 @@
+package ooplab9;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Person person = new Person("Piyanut","1234567890119",
+                new Address("115 M.3","SuratThani","84110"),
+                new Job("private business","30000"));
+
+
+        System.out.println(person.toString());
+
+    }//main
+
+}//class
